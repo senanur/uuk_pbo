adc92ca763f368149f3c8de8386d0d16b4b4dcf8
itszackbabyy <zakyx1ggg@gmail.com>
2023-11-21 08:55:07 +0800

    nambah

---
+++ ahmad_zaky.UUK_PBO/src/ahmad_zaky/uuk_pbo/Ahmad_zakyUUK_PBO.java
@@ -0,0 +1,80 @@
+
+package ahmad_zaky.uuk_pbo;
+
+import java.util.Scanner;
+public class Ahmad_zakyUUK_PBO {
+
+   
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       Scanner input = new Scanner (System.in);
+       int pilihan;
+       int hasil;
+       
+       
+       
+        System.out.print("Masukan bilangan pertama: ");
+        int angka1 = input.nextInt();
+        System.out.print("Masukan bilangan kedua  : ");
+        int angka2 = input.nextInt();
+        
+           do {
+               System.out.println("==========silahkan pilih==========");
+            System.out.println("1. perjumlahan:");
+            System.out.println("1. pengurangan");
+            System.out.println("3. perkalian");
+            System.out.println("4. pembagian");
+            System.out.println("5. Keluar");
+            System.out.print("Masukkan pilihan (1-5): ");
+            pilihan = scanner.nextInt();
+                      
+            
+            switch (pilihan) {
+                case 1:
+                    System.out.println("perjumlahan");
+                    System.out.println(angka1);
+                    System.out.println(angka2);
+                    hasil = angka1 + angka2;
+                    System.out.println("hasil perjumlahan  :  " + hasil);
+                    break;
+                    
+                  case 2:
+                    System.out.println("pengurangan");
+                    System.out.println(angka1);
+                    System.out.println(angka2);
+                    hasil = angka1 - angka2;
+                    System.out.println("hasil perjumlahan  :  " + hasil);
+                    break;
+                    
+                  case 3:
+                    System.out.println("perkalian");
+                    System.out.println(angka1);
+                    System.out.println(angka2);
+                    hasil = angka1 * angka2;
+                    System.out.println("hasil perjumlahan  :  " + hasil);
+                    break;
+                    
+                   case 4:
+                    System.out.println("pembagian");
+                    System.out.println(angka1);
+                    System.out.println(angka2);
+                    hasil = angka1 / angka2;
+                    System.out.println("hasil perjumlahan  :  " + hasil);
+                    break;
+                    
+                     case 5:
+                         System.out.println("terimakasih telah memakai kalkulator sederahana");
+                    break;
+            }
+              
+            
+        
+        
+           
+       
+       
+//       
+           }while (pilihan != 5);
+            }
+    
+}
